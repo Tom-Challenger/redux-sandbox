1195be9894adb50c6b440b810a821ed28178b407
Tom-Challenger <it.mail.art@gmail.com>
2019-08-08 15:37:59 +0300

    Реализация паттерна Reducer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Hello World!')
+//Начальное значение можно передавать в пераметре по-умолчанию
+//const initialState = 0
+
+// Если значение поля .type не удалось распознать,
+// то state возвращается неизменно
+
+// Если state изначально не задан,
+// то он определяется значением по-умолчанию
+
+const reduser = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1
+
+    default:
+      return state
+  }
+}
+
+let state = reduser( undefined, {})
+console.log(state)
+
+state = reduser(state, {type: 'INC'})
+console.log(state)
+
+state = reduser(state, {type: 'INC'})
+console.log(state)
