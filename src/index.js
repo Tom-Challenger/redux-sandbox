1ead90c76843d8c9f230bb70a425a6bd2f471a20
Tom-Challenger <it.mail.art@gmail.com>
2019-08-08 18:47:24 +0300

    Применение Action Creator

--- src/index.js
+++ src/index.js
@@ -25,27 +25,28 @@ const reduser = (state = 0, action) => {
 
 const store = createStore(reduser)
 
+const inc = () => ( { type: 'INC' } )
+const dec = () => ( { type: 'DEC' } )
+const rnd = (payload) =>  ( { type: 'RND', payload } )
+
 document
   .getElementById('inc')
   .addEventListener('click', () => {
-    store.dispatch({type:'INC'})
+    store.dispatch(inc())
   })
 
 
 document
   .getElementById('dec')
   .addEventListener('click', () => {
-    store.dispatch({type:'DEC'})
+    store.dispatch(dec())
   })
 
 document
   .getElementById('rnd')
   .addEventListener('click', () => {
     const payload = Math.floor(Math.random()*10)
-    store.dispatch({
-      type:'RND',
-      payload
-    })
+    store.dispatch(rnd(payload))
   })
 
 const update = () => {
